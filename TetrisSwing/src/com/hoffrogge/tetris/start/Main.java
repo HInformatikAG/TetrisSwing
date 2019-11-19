4e976d21b918a41327e741d06eeb8617105b84d5
Olaf Roeder <37211000+olaf1789@users.noreply.github.com>
2019-11-19 11:38:23 +0100

    Main geleert für die Abschlussaufgabe;

--- TetrisSwing/src/com/hoffrogge/tetris/start/Main.java
+++ TetrisSwing/src/com/hoffrogge/tetris/start/Main.java
@@ -1,72 +1,49 @@
 package com.hoffrogge.tetris.start;
 
-import java.awt.event.KeyListener;
-
-import com.hoffrogge.tetris.logik.Spiel;
-import com.hoffrogge.tetris.model.tetromino.StandardTetrominoFactory;
-import com.hoffrogge.tetris.model.tetromino.TetrominoFactory;
-import com.hoffrogge.tetris.view.Spielfeld;
-import com.hoffrogge.tetris.view.Spielfenster;
-import com.hoffrogge.tetris.view.TetrisKeyListener;
-import com.hoffrogge.tetris.view.Vorschau;
-
 public class Main {
 
     public static void main(String[] args) {
 
         /*
-         * ====================================================================
-         * Diese Factory ist dafür zuständig, Spielsteine zu generieren. Mit
-         * deiner eigenen Factory kannst du hier deine eigenen Spielsteine
-         * einbauen.
-         * ====================================================================
+         * AUFGABE: Instanziiere Objekte der hier genannten Klassen mit den
+         * jeweils benötigten Parametern. Sind alle Objekte korrekt erstellt,
+         * kann das Spiel gestartet werden
          */
-        TetrominoFactory tetrominoFactory = new StandardTetrominoFactory();
+
+        /* Tetris benötigt eine Factory zum Erstellen von Spielsteinen */
+
+        /* Das Spielfeld von Tetris muss als Parameter die Factory bekommen */
 
         /*
-         * Dies ist das Spielfeld. Es zeichnet das Spielfeld und die
-         * Spielsteine. Das Spielfeld kann Tetrisspielsteine nicht beeinflussen.
+         * Es gibt eine Vorschau, auch sie muss die Factory als Parameter
+         * bekommen
          */
-        Spielfeld spielfeld = new Spielfeld(tetrominoFactory);
 
         /*
-         * Die Vorschau zeigt den jeweils nächsten Spielstein an. Mehr kann sie
-         * nicht tun.
+         * Das Spielfenster benötigt als Parameter das Spielfeld und die
+         * Vorschau
          */
-        Vorschau vorschau = new Vorschau(tetrominoFactory);
-
-        /*
-         * Das Spielfenster zeichnet das Spielfeld, die Vorschau, Highscore,
-         * Level, Punkte und Reihen. Das Spielfenster kann Spielsteine nicht
-         * beeinflussen.
-         */
-        Spielfenster spielfenster = new Spielfenster(spielfeld, vorschau);
 
         /*
          * Das Spiel enthält alles an Logik, die es braucht, z. B. das Drehen
          * von Spielsteinen oder die Berechnung von Punkten. Das Spiel kann
          * nichts darstellen, das ist Aufgabe des Spielfelds. Das Spiel kann nur
          * dem Spielfeld Informationen geben, die das Spielfeld dann darstellt.
+         * 
+         * Das Spiel benötigt die Factory, das Spielfeld, das Spielfenster und
+         * die Vorschau als Parameter
          */
-        Spiel spiel = new Spiel(tetrominoFactory, spielfeld, spielfenster, vorschau);
 
         /*
-         * ====================================================================
-         * Dieser Listener wird fuer die Steuerung des Spiels benoetigt (links,
-         * rechts, runter, drehen, Pause). Er erkennt Tastatureingaben und
-         * reicht diese an das Spiel weiter. Ohne den KeyListener läuft das
-         * Spiel, ohne das der Spieler etwas machen kann. Hier musst du deinen
-         * KeyListener einbauen.
-         * ====================================================================
+         * Der TetrisKeyListener sorgt für die Steuerung und benötigt das Spiel
+         * als Parameter.
          */
-        KeyListener tetrisKeyListener = new TetrisKeyListener(spiel);
 
         /*
-         * Der KeyListener muss an einer Komponente hängen, die angezeigt wird,
-         * in diesem Fall eignet sich das Spielfenster dafür.
+         * Das Spielfenster benötigt jetzt noch den KeyListener, dieser muss
+         * hinzugefügt werden
          */
-        spielfenster.addKeyListener(tetrisKeyListener);
 
-        spiel.starteSpiel();
+        /* jetzt kann das Spiel gestartet werden. */
     }
 }
