be08b3fe2ccb4be91732afd16ed30519e7e63389
Olaf Roeder <github@olaf-roeder.de>
2020-02-08 00:53:11 +0100

    Punkt braucht keine Setter;

--- TetrisSwing/src/com/hoffrogge/tetris/model/Punkt.java
+++ TetrisSwing/src/com/hoffrogge/tetris/model/Punkt.java
@@ -10,23 +10,15 @@ public class Punkt {
     private int y;
 
     public Punkt(int x, int y) {
-        this.x = x;
-        this.y = y;
+	this.x = x;
+	this.y = y;
     }
 
     public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
+	return x;
     }
 
     public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
+	return y;
     }
 }
