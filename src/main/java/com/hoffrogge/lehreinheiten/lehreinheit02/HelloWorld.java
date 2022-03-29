32febf06a1b2dd8da2ba9470c09277a45a138d98
Olaf Roeder <37211000+olaf1789@users.noreply.github.com>
2022-03-29 09:18:29 +0200

    Quellcode formatiert

--- src/main/java/com/hoffrogge/lehreinheiten/lehreinheit02/HelloWorld.java
+++ src/main/java/com/hoffrogge/lehreinheiten/lehreinheit02/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 
     /* Startpunkt des Programms */
     public static void main(String[] args) {
-	System.out.println("Hallo, Welt :)");
+        System.out.println("Hallo, Welt :)");
     }
 }
