0a2040613242831e4c8facc5515349df54daeaf7
Olaf Roeder <37211000+olaf1789@users.noreply.github.com>
2023-03-06 12:13:43 +0100

    Aufgabe zur Übung hinzugefügt & formatiert;

--- src/main/java/com/hoffrogge/lehreinheiten/lehreinheit02/Wuerfel.java
+++ src/main/java/com/hoffrogge/lehreinheiten/lehreinheit02/Wuerfel.java
@@ -22,13 +22,13 @@ public class Wuerfel {
      * dieser Klasse, ohne Variablen zu setzen.
      */
     public Wuerfel() {
-	/*
-	 * Variablen werden nicht gesetzt, d. h. sie haben folgende Werte:
-	 *
-	 * anzahlSeiten ist 0 (Standardwert fuer int)
-	 *
-	 * beschreibung ist null (Standardwert fuer String)
-	 */
+        /*
+         * Variablen werden nicht gesetzt, d. h. sie haben folgende Werte:
+         *
+         * anzahlSeiten ist 0 (Standardwert fuer int)
+         *
+         * beschreibung ist null (Standardwert fuer String)
+         */
     }
 
     /*
@@ -36,13 +36,13 @@ public class Wuerfel {
      * zuweist.
      */
     public Wuerfel(int anzahlSeiten) {
-	/*
-	 * der Wert von anzahlSeiten in diesem Objekt ("this.anzahlSeiten") wird auf den
-	 * Wert des Parameters gesetzt ("anzahlSeiten" ohne this)
-	 */
-	this.anzahlSeiten = anzahlSeiten;
+        /*
+         * der Wert von anzahlSeiten in diesem Objekt ("this.anzahlSeiten") wird auf den
+         * Wert des Parameters gesetzt ("anzahlSeiten" ohne this)
+         */
+        this.anzahlSeiten = anzahlSeiten;
 
-	/* beschreibung ist immer noch null */
+        /* beschreibung ist immer noch null */
     }
 
     /*
@@ -50,15 +50,15 @@ public class Wuerfel {
      * zuweist.
      */
     public Wuerfel(String beschreibung) {
-	this.beschreibung = beschreibung;
+        this.beschreibung = beschreibung;
 
-	/* anzahlSeiten ist hier noch 0 */
+        /* anzahlSeiten ist hier noch 0 */
     }
 
     /* Konstruktor, der beide Variablen fuer das erstellte Objekt setzt */
     public Wuerfel(int anzahlSeiten, String beschreibung) {
-	this.anzahlSeiten = anzahlSeiten;
-	this.beschreibung = beschreibung;
+        this.anzahlSeiten = anzahlSeiten;
+        this.beschreibung = beschreibung;
     }
 
     /*
@@ -66,18 +66,18 @@ public class Wuerfel {
      * und zu aendern
      */
     public int getAnzahlSeiten() {
-	return anzahlSeiten;
+        return anzahlSeiten;
     }
 
     public void setAnzahlSeiten(int anzahlSeiten) {
-	this.anzahlSeiten = anzahlSeiten;
+        this.anzahlSeiten = anzahlSeiten;
     }
 
     public String getBeschreibung() {
-	return beschreibung;
+        return beschreibung;
     }
 
     public void setBeschreibung(String beschreibung) {
-	this.beschreibung = beschreibung;
+        this.beschreibung = beschreibung;
     }
 }
