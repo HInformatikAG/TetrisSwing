a777dcb69ef3f94b17b8066974e2c25c0a3037de
o.roeder <37211000+olaf1789@users.noreply.github.com>
2025-03-07 08:53:26 +0100

    Formatierung des Quelltextes;

--- src/main/java/com/hoffrogge/lehreinheiten/lehreinheit02/VariablenScope.java
+++ src/main/java/com/hoffrogge/lehreinheiten/lehreinheit02/VariablenScope.java
@@ -21,7 +21,7 @@ public class VariablenScope {
      *
      * Klassenvariablen sind fuer alle Objekte einer Klasse gleich.
      */
-    private static int klassenVariable;
+    private static int klassenVariable = 0;
 
     /*
      * Objektvariablen/Instanzvariablen werden am Anfang der Klasse definiert, sie
@@ -38,17 +38,17 @@ public class VariablenScope {
     /* Konstruktor */
     public VariablenScope(int klassenVariable, int objektVariable) {
 
-	/*
-	 * Auf Klassenvariablen wird mit der Punktnotation ueber die Klasse zugegriffen.
-	 * (Oder ueber setter und getter)
-	 */
-	VariablenScope.klassenVariable = klassenVariable;
+        /*
+         * Auf Klassenvariablen wird mit der Punktnotation ueber die Klasse zugegriffen.
+         * (Oder ueber Getter und Setter)
+         */
+        VariablenScope.klassenVariable = klassenVariable;
 
-	/*
-	 * Auf Objektvariablen wird mit dem Keyword "this" und der Punktnotation
-	 * zugegriffen. (Oder ueber setter und getter)
-	 */
-	this.objektVariable = objektVariable;
+        /*
+         * Auf Objektvariablen wird mit dem Keyword "this" und der Punktnotation
+         * zugegriffen. (Oder ueber Getter und Setter)
+         */
+        this.objektVariable = objektVariable;
     }
 
     /*
@@ -56,52 +56,52 @@ public class VariablenScope {
      * immer gleich!
      */
     public void printKlassenVariable() {
-	System.out.println(klassenVariable);
+        System.out.println(klassenVariable);
     }
 
     /*
      * Der Wert von objektVariable kann fuer jedes Objekt unterschiedlich sein!
      */
     public void printObjektVariable() {
-	System.out.println(objektVariable);
+        System.out.println(objektVariable);
     }
 
     /* Gibt die Summer der klassenVariable und der objektVariable aus */
     public void printSumme() {
 
-	/*
-	 * Dies ist eine lokale Variable.
-	 * 
-	 * Lokale Variablen koennen nur innerhalb der Methode benutzt werden, in der sie
-	 * definiert werden.
-	 */
-	int lokaleSumme = klassenVariable + objektVariable;
+        /*
+         * Dies ist eine lokale Variable.
+         *
+         * Lokale Variablen koennen nur innerhalb der Methode benutzt werden, in der sie
+         * definiert werden.
+         */
+        int lokaleSumme = klassenVariable + objektVariable;
 
-	System.out.println("Summe: " + lokaleSumme);
+        System.out.println("Summe: " + lokaleSumme);
     }
 
     public static void main(String[] args) {
 
-	VariablenScope objektA = new VariablenScope(7, 11);
-	System.out.print("objektA klassenVariable ");
-	objektA.printKlassenVariable();
-	System.out.print("objektA objektVariable ");
-	objektA.printObjektVariable();
+        VariablenScope objektA = new VariablenScope(7, 11);
+        System.out.print("objektA klassenVariable ");
+        objektA.printKlassenVariable();
+        System.out.print("objektA objektVariable ");
+        objektA.printObjektVariable();
 
-	// nur Leerzeile
-	System.out.println();
+        // nur Leerzeile
+        System.out.println();
 
-	VariablenScope objektB = new VariablenScope(77, 1111);
-	System.out.print("objektB klassenVariable ");
-	objektB.printKlassenVariable();
-	System.out.print("objektB objektVariable ");
-	objektB.printObjektVariable();
+        VariablenScope objektB = new VariablenScope(77, 1111);
+        System.out.print("objektB klassenVariable ");
+        objektB.printKlassenVariable();
+        System.out.print("objektB objektVariable ");
+        objektB.printObjektVariable();
 
-	System.out.println();
+        System.out.println();
 
-	System.out.print("objektA klassenVariable ");
-	objektA.printKlassenVariable();
-	System.out.print("objektA objektVariable ");
-	objektA.printObjektVariable();
+        System.out.print("objektA klassenVariable ");
+        objektA.printKlassenVariable();
+        System.out.print("objektA objektVariable ");
+        objektA.printObjektVariable();
     }
 }
