1cb8ccf9c5bf3e859361a229b8acdec6ca6b8a25
Olaf Roeder <37211000+olaf1789@users.noreply.github.com>
2026-03-02 14:28:50 +0100

    TODO entfernt, Kommentar ergänzt;

--- src/main/java/com/hoffrogge/tetris/view/Spielfenster.java
+++ src/main/java/com/hoffrogge/tetris/view/Spielfenster.java
@@ -9,135 +9,136 @@ import java.net.URL;
 @SuppressWarnings("serial")
 public class Spielfenster extends JFrame {
 
-	private JLabel levelWertLabel;
-	private JLabel reihenWertLabel;
-	private JLabel punkteWertLabel;
+    private JLabel levelWertLabel;
+    private JLabel reihenWertLabel;
+    private JLabel punkteWertLabel;
 
-	private JLabel highscoreWertLabel;
+    private JLabel highscoreWertLabel;
 
-	public Spielfenster(Spielfeld spielfeld, Vorschau vorschau) {
+    public Spielfenster(Spielfeld spielfeld, Vorschau vorschau) {
 
-		getContentPane().add(spielfeld);
+        getContentPane().add(spielfeld);
 
-		getContentPane().add(vorschau);
+        getContentPane().add(vorschau);
 
-		getContentPane().setBackground(TetrisKonstanten.HINTERGRUND);
-		getContentPane().setForeground(TetrisKonstanten.VORDERGRUND);
+        getContentPane().setBackground(TetrisKonstanten.HINTERGRUND);
+        getContentPane().setForeground(TetrisKonstanten.VORDERGRUND);
 
-		setTitle("Hoffrogge Tetris Klon");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Hoffrogge Tetris Klon");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		setSize(new Dimension(TetrisKonstanten.FENSTER_BREITE, TetrisKonstanten.FENSTER_HOEHE));
+        setSize(new Dimension(TetrisKonstanten.FENSTER_BREITE, TetrisKonstanten.FENSTER_HOEHE));
 
-		setResizable(false);
-		setLocationRelativeTo(null);
-		getContentPane().setLayout(null);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        getContentPane().setLayout(null);
 
-		Font font = new Font("Arial Black", Font.TRUETYPE_FONT, (int) (TetrisKonstanten.BLOCK_BREITE / 2.2));
+        Font font = new Font("Arial Black", Font.TRUETYPE_FONT, (int) (TetrisKonstanten.BLOCK_BREITE / 2.2));
 
-		JLabel levelLabel = new JLabel("Level");
-		levelLabel.setFont(font);
-		levelLabel.setBounds(TetrisKonstanten.LABEL_LINKS_POS_X, TetrisKonstanten.LABEL_LINKS_POS_Y,
-				TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
-		levelLabel.setForeground(TetrisKonstanten.TEXT);
-		getContentPane().add(levelLabel);
+        JLabel levelLabel = new JLabel("Level");
+        levelLabel.setFont(font);
+        levelLabel.setBounds(TetrisKonstanten.LABEL_LINKS_POS_X, TetrisKonstanten.LABEL_LINKS_POS_Y,
+                TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
+        levelLabel.setForeground(TetrisKonstanten.TEXT);
+        getContentPane().add(levelLabel);
 
-		JLabel reihenLabel = new JLabel("Reihen");
-		reihenLabel.setFont(font);
-		reihenLabel.setBounds(TetrisKonstanten.LABEL_LINKS_POS_X,
-				TetrisKonstanten.LABEL_LINKS_POS_Y + TetrisKonstanten.BLOCK_BREITE,
-				TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
-		reihenLabel.setForeground(TetrisKonstanten.TEXT);
-		getContentPane().add(reihenLabel);
+        JLabel reihenLabel = new JLabel("Reihen");
+        reihenLabel.setFont(font);
+        reihenLabel.setBounds(TetrisKonstanten.LABEL_LINKS_POS_X,
+                TetrisKonstanten.LABEL_LINKS_POS_Y + TetrisKonstanten.BLOCK_BREITE,
+                TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
+        reihenLabel.setForeground(TetrisKonstanten.TEXT);
+        getContentPane().add(reihenLabel);
 
-		JLabel punkteLabel = new JLabel("Punkte");
-		punkteLabel.setFont(font);
-		punkteLabel.setBounds(TetrisKonstanten.LABEL_LINKS_POS_X,
-				TetrisKonstanten.LABEL_LINKS_POS_Y + TetrisKonstanten.BLOCK_BREITE * 2,
-				TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
-		punkteLabel.setForeground(TetrisKonstanten.TEXT);
-		getContentPane().add(punkteLabel);
+        JLabel punkteLabel = new JLabel("Punkte");
+        punkteLabel.setFont(font);
+        punkteLabel.setBounds(TetrisKonstanten.LABEL_LINKS_POS_X,
+                TetrisKonstanten.LABEL_LINKS_POS_Y + TetrisKonstanten.BLOCK_BREITE * 2,
+                TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
+        punkteLabel.setForeground(TetrisKonstanten.TEXT);
+        getContentPane().add(punkteLabel);
 
-		JLabel lblHighscore = new JLabel("Highscore");
-		lblHighscore.setForeground(TetrisKonstanten.TEXT);
-		lblHighscore.setFont(font);
-		lblHighscore.setBounds(TetrisKonstanten.LABEL_LINKS_POS_X,
-				TetrisKonstanten.LABEL_LINKS_POS_Y + TetrisKonstanten.BLOCK_BREITE * 3,
-				TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
-		getContentPane().add(lblHighscore);
+        JLabel lblHighscore = new JLabel("Highscore");
+        lblHighscore.setForeground(TetrisKonstanten.TEXT);
+        lblHighscore.setFont(font);
+        lblHighscore.setBounds(TetrisKonstanten.LABEL_LINKS_POS_X,
+                TetrisKonstanten.LABEL_LINKS_POS_Y + TetrisKonstanten.BLOCK_BREITE * 3,
+                TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
+        getContentPane().add(lblHighscore);
 
-		levelWertLabel = new JLabel();
-		levelWertLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		levelWertLabel.setFont(font);
-		levelWertLabel.setBounds(TetrisKonstanten.LABEL_RECHTS_POS_X, TetrisKonstanten.LABEL_RECHTS_POS_Y,
-				TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
-		levelWertLabel.setForeground(TetrisKonstanten.TEXT);
-		getContentPane().add(levelWertLabel);
+        levelWertLabel = new JLabel();
+        levelWertLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        levelWertLabel.setFont(font);
+        levelWertLabel.setBounds(TetrisKonstanten.LABEL_RECHTS_POS_X, TetrisKonstanten.LABEL_RECHTS_POS_Y,
+                TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
+        levelWertLabel.setForeground(TetrisKonstanten.TEXT);
+        getContentPane().add(levelWertLabel);
 
-		reihenWertLabel = new JLabel();
-		reihenWertLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		reihenWertLabel.setFont(font);
-		reihenWertLabel.setBounds(TetrisKonstanten.LABEL_RECHTS_POS_X,
-				TetrisKonstanten.LABEL_RECHTS_POS_Y + TetrisKonstanten.BLOCK_BREITE,
-				TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
-		reihenWertLabel.setForeground(TetrisKonstanten.TEXT);
-		getContentPane().add(reihenWertLabel);
+        reihenWertLabel = new JLabel();
+        reihenWertLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        reihenWertLabel.setFont(font);
+        reihenWertLabel.setBounds(TetrisKonstanten.LABEL_RECHTS_POS_X,
+                TetrisKonstanten.LABEL_RECHTS_POS_Y + TetrisKonstanten.BLOCK_BREITE,
+                TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
+        reihenWertLabel.setForeground(TetrisKonstanten.TEXT);
+        getContentPane().add(reihenWertLabel);
 
-		punkteWertLabel = new JLabel();
-		punkteWertLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		punkteWertLabel.setFont(font);
-		punkteWertLabel.setBounds(TetrisKonstanten.LABEL_RECHTS_POS_X,
-				TetrisKonstanten.LABEL_RECHTS_POS_Y + TetrisKonstanten.BLOCK_BREITE * 2,
-				TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
-		punkteWertLabel.setForeground(TetrisKonstanten.TEXT);
-		getContentPane().add(punkteWertLabel);
+        punkteWertLabel = new JLabel();
+        punkteWertLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        punkteWertLabel.setFont(font);
+        punkteWertLabel.setBounds(TetrisKonstanten.LABEL_RECHTS_POS_X,
+                TetrisKonstanten.LABEL_RECHTS_POS_Y + TetrisKonstanten.BLOCK_BREITE * 2,
+                TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
+        punkteWertLabel.setForeground(TetrisKonstanten.TEXT);
+        getContentPane().add(punkteWertLabel);
 
-		highscoreWertLabel = new JLabel();
-		highscoreWertLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		highscoreWertLabel.setForeground(TetrisKonstanten.TEXT);
-		highscoreWertLabel.setFont(font);
-		highscoreWertLabel.setBounds(TetrisKonstanten.LABEL_RECHTS_POS_X,
-				TetrisKonstanten.LABEL_RECHTS_POS_Y + TetrisKonstanten.BLOCK_BREITE * 3,
-				TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
-		getContentPane().add(highscoreWertLabel);
+        highscoreWertLabel = new JLabel();
+        highscoreWertLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        highscoreWertLabel.setForeground(TetrisKonstanten.TEXT);
+        highscoreWertLabel.setFont(font);
+        highscoreWertLabel.setBounds(TetrisKonstanten.LABEL_RECHTS_POS_X,
+                TetrisKonstanten.LABEL_RECHTS_POS_Y + TetrisKonstanten.BLOCK_BREITE * 3,
+                TetrisKonstanten.VORSCHAU_BREITE / 2, TetrisKonstanten.BLOCK_BREITE);
+        getContentPane().add(highscoreWertLabel);
 
-		Font steuerungFont = new Font("Arial", Font.PLAIN, TetrisKonstanten.BLOCK_BREITE / 4);
-		JLabel lblPauseMitP = new JLabel(
-				"<html><p align=\"right\">Pause mit P <br> Drehen mit Q, E <br> Bewegen mit Pfeiltasten</p></html>");
-		lblPauseMitP.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblPauseMitP.setForeground(TetrisKonstanten.TEXT);
-		lblPauseMitP.setFont(steuerungFont);
-		lblPauseMitP.setBounds(TetrisKonstanten.SPIELFELD_BREITE + TetrisKonstanten.ABSTAND * 2,
-				TetrisKonstanten.SPIELFELD_HOEHE - TetrisKonstanten.ABSTAND, TetrisKonstanten.VORSCHAU_BREITE,
-				TetrisKonstanten.BLOCK_BREITE);
-		getContentPane().add(lblPauseMitP);
+        Font steuerungFont = new Font("Arial", Font.PLAIN, TetrisKonstanten.BLOCK_BREITE / 4);
+        JLabel lblPauseMitP = new JLabel(
+                "<html><p align=\"right\">Pause mit P <br> Drehen mit Q, E <br> Bewegen mit Pfeiltasten</p></html>");
+        lblPauseMitP.setHorizontalAlignment(SwingConstants.RIGHT);
+        lblPauseMitP.setForeground(TetrisKonstanten.TEXT);
+        lblPauseMitP.setFont(steuerungFont);
+        lblPauseMitP.setBounds(TetrisKonstanten.SPIELFELD_BREITE + TetrisKonstanten.ABSTAND * 2,
+                TetrisKonstanten.SPIELFELD_HOEHE - TetrisKonstanten.ABSTAND, TetrisKonstanten.VORSCHAU_BREITE,
+                TetrisKonstanten.BLOCK_BREITE);
+        getContentPane().add(lblPauseMitP);
 
-		URL resource = getClass().getResource("tetrisIcon.png");
+        URL resource = getClass().getResource("tetrisIcon.png");
 
-		ImageIcon icon = new ImageIcon(resource);
+        ImageIcon icon = new ImageIcon(resource);
 
-		setIconImage(icon.getImage());
+        setIconImage(icon.getImage());
 
-		setVisible(true);
+        setVisible(true);
 
-		//TODO das hier sollte doch eher in den entsprechenden Klassen geschehen und nicht an dieser Stelle hier?
-		spielfeld.createBufferStrategy(2);
-		vorschau.createBufferStrategy(2);
-	}
+        // sollte doch eher in den entsprechenden Klassen geschehen und nicht an dieser Stelle hier,
+        // aber solange Spielfeld und Vorschau nicht sichtbar sind, kann man createBufferStrategy nicht aufrufen
+        spielfeld.createBufferStrategy(2);
+        vorschau.createBufferStrategy(2);
+    }
 
-	public void setLevel(String level) {
-		levelWertLabel.setText(level);
-	}
+    public void setLevel(String level) {
+        levelWertLabel.setText(level);
+    }
 
-	public void setPunkte(String punkte) {
-		punkteWertLabel.setText(punkte);
-	}
+    public void setPunkte(String punkte) {
+        punkteWertLabel.setText(punkte);
+    }
 
-	public void setReihen(String reihen) {
-		reihenWertLabel.setText(reihen);
-	}
+    public void setReihen(String reihen) {
+        reihenWertLabel.setText(reihen);
+    }
 
-	public void setHighscore(String highscore) {
-		highscoreWertLabel.setText(highscore);
-	}
+    public void setHighscore(String highscore) {
+        highscoreWertLabel.setText(highscore);
+    }
 }
